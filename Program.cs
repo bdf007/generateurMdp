48d4182d7b3016bf24d7843c4a3dedc705a6e2d2
bdf007 <bdf007@gmail.com>
2023-12-06 11:09:18 +0100

    programme complet

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using FormationCS;
+
+namespace generateurMdp
+{
+    class Program
+    {
+        static string genererMdp ()
+        {
+            int nombreCaractere = outils.DemanderNombrePositifNonNul($"Choisir la longueur du mot de passe  : ");
+            Console.WriteLine($"nombre de caracteres choisi : {nombreCaractere}");
+            string alphabet = "";
+            string minuscule = "abcdefghijklmnopqrstuvwxyz";
+            string majuscule = minuscule.ToUpper();
+            string chiffre = "0123456789";
+            string symbole = "!&#*+";
+            int typeDeMdp = 0;
+            while (typeDeMdp <= 0)
+            {
+                typeDeMdp = outils.DemanderNombrePositifNonNul("quel type de mot passe souhaitée?\n" +
+                "1 - uniquement des caratères en minuscule\n" +
+                "2 - Des catactères minuscules et majuscules\n" +
+                "3 - Des caractères et des chiffres\n" +
+                "4 - Caractères, chiffres et caractères spéciaux\n" +
+                "");
+                switch (typeDeMdp)
+                {
+                    case 1:
+                        alphabet = minuscule;
+                        break;
+                    case 2:
+                        alphabet = minuscule + majuscule;
+                        break;
+                    case 3:
+                        alphabet = minuscule + majuscule + chiffre;
+                        break;
+                    case 4:
+                        alphabet = minuscule + majuscule + chiffre + symbole;
+                        break;
+                    default:
+                        Console.WriteLine("Erreur : Vous devez saisir un nombre entre 1 et 4");
+                        typeDeMdp = 0;
+
+                        break;
+                }
+            }
+
+            Random rand = new Random();
+            string mdp = "";
+            for (int i = 0; i < nombreCaractere; i++)
+            {
+                int index = rand.Next(0, alphabet.Length);
+                mdp += alphabet[index];
+            }
+            return mdp;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans le générateur de mot de passe");
+            string motDePasse= genererMdp();
+            Console.WriteLine(motDePasse);
+
+        }
+    }
+}
---
+++ outils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+
+            return DemanderNombreEntre(question, 1, int.MaxValue, "Erreur : Le nombre doit être supérieur à zéro");
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonalise = "")
+        {
+            int nombre = DemanderNombre(question);
+            while (nombre < min || nombre > max)
+            {
+                if (messageErreurPersonalise != "")
+                {
+                    Console.WriteLine(messageErreurPersonalise);
+                }
+                else
+                {
+
+                Console.WriteLine($"Erreur : Vous devez saisir un nombre entre {min} et {max}");
+                }
+                Console.WriteLine();
+                nombre = DemanderNombre(question);
+            }
+            return nombre;
+        }
+        static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Erreur : Vous devez saisir un nombre");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
